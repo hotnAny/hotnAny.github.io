f000885445ccd07ef2b382887f79e7eca372026a
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2014-12-09 20:40:54 -0500

    added object manipulation using mouse

--- sandbox/xacInit.js
+++ sandbox/xacInit.js
@@ -1,3 +1,13 @@
+// for debugging
+var D = false;
+var ball;
+
+var objects = new Array();
+var selected = new Array();
+
+var colorNormal = 0xffffff;
+var colorSelected = 0x00ff00;
+
 var renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -6,7 +16,6 @@ var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 10000 );
 camera.position.set(-0, 180, 200);
-camera.rotation.x -= Math.PI / 6;
 
 var controls = new THREE.TrackballControls( camera ); // for mouse control
 
--- sandbox/xacLoadModel.js
+++ sandbox/xacLoadModel.js
@@ -4,28 +4,30 @@ var tree = 'things/xmas-tree-v1.stl';
 var chess = 'things/classic_bishop.stl';
 var bunny = 'things/bunny.stl';
 
+function loadObject (objName) {
+  loader.load(objName, function (geometry) {
+    // center the object
+    geometry.center();
 
-loader.load(bunny, function (geometry) {
+    var material = new THREE.MeshPhongMaterial( { color: colorNormal} );  
+    var object = new THREE.Mesh(geometry, material); 
+    object.rotation.x = -Math.PI/2;
+    scene.add(object);
+    
+    objects.push(object);
   
-  // center the object
-  // THREE.GeometryUtils.center( geometry );
-  geometry.center();
+  });
+}
+
+if(D) {
+  var geometry = new THREE.SphereGeometry( 5, 20, 20 );
+  var material = new THREE.MeshBasicMaterial( { color: 0xf0ff00 } );
+  ball = new THREE.Mesh( geometry, material );
   
-  // var material = new THREE.LineDashedMaterial( { linewidth:0.1 } );
-
-  var material = new THREE.MeshPhongMaterial( { color: 0xffffff} );
-  // var material = new THREE.MeshNormalMaterial( {color: 0x000000, shading: THREE.SmoothShading} );
-
-  // var material = new THREE.MeshPhongMaterial( { color: 0x000000, specular: 0x666666, emissive: 0xff0000, ambient: 0x00ff00, shininess: 50, 
-  //   shading: THREE.SmoothShading, opacity: 0.9, transparent: true } );
-
-  var mesh = new THREE.Mesh(geometry, material); 
-  mesh.rotation.x = -Math.PI/2;
-  scene.add(mesh);
-  // meshes.push(mesh);
-
-  camera.position.set(-0, 180, 200);
-  camera.rotation.x -= Math.PI / 6;
-  
-});
+  scene.add( ball );
+  objects.push(ball);
+} else {
+  loadObject(bunny);
+  // loadObject(chess);
+}
 
