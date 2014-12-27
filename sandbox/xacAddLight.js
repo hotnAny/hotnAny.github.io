dca192ad86663b6375b407ef62c621c48d3e4a58
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2014-12-27 16:27:40 -0500

    clear everything

--- sandbox/xacAddLight.js
+++
@@ -1,7 +0,0 @@
-// directional light
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-directionalLight.position.set( 1, 1, 1 );
-scene.add( directionalLight );
-
-// ambient light
-scene.add( new THREE.AmbientLight( 0x888888 ) );
--- sandbox/xacDrawGround.js
+++
@@ -1,6 +0,0 @@
-var groundMaterial = new THREE.MeshBasicMaterial( { color: 0x030303 } );
-var geometryGround = new THREE.PlaneBufferGeometry( 1000, 1000, 25, 25 );
-
-var ground = new THREE.Mesh( geometryGround, groundMaterial );
-ground.rotation.x -= Math.PI / 2;
-scene.add( ground );
