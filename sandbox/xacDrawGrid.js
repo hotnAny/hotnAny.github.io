b496ab8d73fc906bd751d0a8c02b5c288cf0b468
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2015-01-07 23:01:42 -0500

    added octree

---
+++ sandbox/xacDrawGrid.js
@@ -0,0 +1,17 @@
+var lineMaterial = new THREE.LineBasicMaterial( { color: 0x606060 } );
+var lineGeometry = new THREE.Geometry();
+var floor = 0;
+var step = 25;
+
+for ( var i = 0; i <= 40; i ++ ) {
+
+	lineGeometry.vertices.push( new THREE.Vector3( - 500, floor, i * step - 500 ) );
+	lineGeometry.vertices.push( new THREE.Vector3(   500, floor, i * step - 500 ) );
+
+	lineGeometry.vertices.push( new THREE.Vector3( i * step - 500, floor, -500 ) );
+	lineGeometry.vertices.push( new THREE.Vector3( i * step - 500, floor,  500 ) );
+
+}
+
+var line = new THREE.Line( lineGeometry, lineMaterial, THREE.LinePieces );
+scene.add( line );
