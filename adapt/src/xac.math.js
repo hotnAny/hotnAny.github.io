b83bc62ab4eb2147f5e0687b1230d83c4487a077
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-03-04 12:01:06 -0500

    updated math library

--- adapt/src/xac.math.js
+++ adapt/src/xac.math.js
@@ -12,7 +12,7 @@ function findPlaneToFitPoints(points) {
 	// package the points into a matrix
 	var G = [];
 
-	for(var i=0; i<points.length; i++) {
+	for (var i = 0; i < points.length; i++) {
 		G.push([points[i].x, points[i].y, points[i].z, 1]);
 	}
 
@@ -24,7 +24,12 @@ function findPlaneToFitPoints(points) {
 	var c = usv.V[2][3];
 	var d = usv.V[3][3];
 
-	return {A:a, B:b, C:c, D:d};
+	return {
+		A: a,
+		B: b,
+		C: c,
+		D: d
+	};
 }
 
 
@@ -32,7 +37,7 @@ function project(points, axis) {
 	var min = 1000;
 	var max = -1000;
 
-	for(var i=0; i<points.length; i++) {
+	for (var i = 0; i < points.length; i++) {
 		var val = axis.dot(points[i]);
 		min = Math.min(min, val);
 		max = Math.max(val, max);
@@ -42,16 +47,20 @@ function project(points, axis) {
 }
 
 
-function getPlaneFromThreePoints(p0, p1, p2) {
-	var A = [	[p0.x, p0.y, p0.z, 1],
-				[p1.x, p1.y, p1.z, 1],
-				[p2.x, p2.y, p2.z, 1]
-			];
-	// var D = numeric.mul(numeric.identity(), -)
-	var b = [0, 0, 0, 0];
-	var LUP = numeric.ccsLUP(A);
-	var p = numeric.ccsLUPSolve(LUP, b);
-	console.log(p);
+function getPlaneFromPointVectors(pt, v1, v2) {
+	var cp = new THREE.Vector3().crossVectors(v1, v2);
+
+	var a = cp.x;
+	var b = cp.y;
+	var c = cp.z;
+	var d = -a * pt.x - b * pt.y - c * pt.z;
+
+	return {
+		A: a,
+		B: b,
+		C: c,
+		D: d
+	};
 }
 
 
@@ -59,6 +68,6 @@ function getPlaneFromThreePoints(p0, p1, p2) {
 	get the projection coordinates of a point on a given plane parameterized by ax+by+cz+d=0
 */
 function getProjection(v, a, b, c, d) {
-	var t = -(a*v.x + b*v.y + c*v.z + d) / (a*a + b*b + c*c);
-	return new THREE.Vector3(v.x + a*t, v.y + b*t, v.z + c*t);
+	var t = -(a * v.x + b * v.y + c * v.z + d) / (a * a + b * b + c * c);
+	return new THREE.Vector3(v.x + a * t, v.y + b * t, v.z + c * t);
 }
