231b080ace39e86f36ad63f3383c1d6e8dca56db
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-05-16 16:46:31 -0400

    minimalize template

---
+++ template-beta/src/x.input.js
@@ -0,0 +1,8 @@
+gMouseCtrls.rotateSpeed = 5.0;
+gMouseCtrls.zoomSpeed = 0.5;
+gMouseCtrls.panSpeed = 2;
+
+gMouseCtrls.noZoom = false;
+
+gMouseCtrls.staticMoving = true;
+gMouseCtrls.dynamicDampingFactor = 0.3;
