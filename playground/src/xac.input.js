00c7bff4802140e0b162d20c664528435773ec36
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-06-13 15:51:02 -0400

    making mainpulation plane orthogonal

--- playground/src/xac.input.js
+++ playground/src/xac.input.js
@@ -1,11 +1,20 @@
 var XAC = XAC || {};
 
-XAC.Maniplane = function(object) {
-	this._object = object;
+XAC.Maniplane = function(pos, orthogonal, showPlane) {
+	// this._object = object;
 	this._camera = camera;
-	this._plane = new XAC.Plane(1000, 1000, XAC.MATERIALINVISIBLE).m;
-	this._plane.position.copy(this._object.position);
-	XAC.rotateObjTo(this._plane, new THREE.Vector3().subVectors(camera.position, this._plane.position));
+	this._plane = new XAC.Plane(1000, 1000, showPlane ? XAC.MATERIALPLAIN : XAC.MATERIALINVISIBLE).m;
+	this._plane.position.copy(pos);
+
+	var vecView = new THREE.Vector3().subVectors(camera.position, this._plane.position);
+	if (orthogonal) {
+		var angleView = new THREE.Vector3(0, 1, 0).angleTo(vecView);
+		if(angleView > Math.PI / 3) {
+			XAC.rotateObjTo(this._plane, vecView);	
+		}
+	} else {
+		XAC.rotateObjTo(this._plane, vecView);
+	}
 
 	scene.add(this._plane);
 };
@@ -16,8 +25,6 @@ XAC.Maniplane.prototype = {
 	},
 
 	destruct: function() {
-		// setTimeout(function() {
 		scene.remove(this._plane);
-		// }, 500);
 	}
 }
