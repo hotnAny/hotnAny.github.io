b23bccf6f0d94984634b544f63ffde97a3c85953
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-06-16 12:46:47 -0400

    added update functions for a few things

--- playground/src/xac.thing.js
+++ playground/src/xac.thing.js
@@ -50,16 +50,27 @@ XAC.Line.prototype = Object.create(XAC.Thing.prototype);
 //
 XAC.ThickLine = function(p1, p2, r, mat) {
 	var h = p1.distanceTo(p2);
-	var line = new XAC.Cylinder(r, h, mat, true);
-	this._g = line.g;
-	this._m = line.m;
+	this._line = new XAC.Cylinder(r, h, mat, true);
+	this._g = this._line.g;
+	this._m = this._line.m;
 
-	var dir = p2.clone().sub(p1);
-	XAC.rotateObjTo(this._m, dir);
+	this._dir = p2.clone().sub(p1);
+	XAC.rotateObjTo(this._m, this._dir);
 	var ctr = p1.clone().add(p2).multiplyScalar(0.5);
 	this._m.position.copy(ctr);
 }
 XAC.ThickLine.prototype = Object.create(XAC.Thing.prototype);
+XAC.ThickLine.prototype.update = function(p1, p2, r) {
+	var h = p1.distanceTo(p2);
+	this._line.update(r, h, undefined, true);
+
+	XAC.rotateObjTo(this._m, this._dir, true);
+	this._dir = p2.clone().sub(p1);
+	XAC.rotateObjTo(this._m, this._dir);
+
+	var ctr = p1.clone().add(p2).multiplyScalar(0.5);
+	this._m.position.copy(ctr);
+}
 
 //
 //	plane
@@ -75,15 +86,25 @@ XAC.Plane.prototype = Object.create(XAC.Thing.prototype);
 //
 XAC.Sphere = function(r, mat, highFi) {
 	this._r = r;
-	this._g = highFi == true ? new THREE.SphereGeometry(r, 32, 32) : new THREE.SphereGeometry(r, 8, 8);
+	this._highFi = highFi;
+	this._g = this._highFi == true ? new THREE.SphereGeometry(r, 32, 32) : new THREE.SphereGeometry(r, 8, 8);
 	this._m = new THREE.Mesh(this._g, mat == undefined ? XAC.MATERIALNORMAL.clone() : mat.clone());
 };
 XAC.Sphere.prototype = Object.create(XAC.Thing.prototype);
+XAC.Sphere.prototype.update = function(r) {
+	this._m.geometry = this._highFi == true ? new THREE.SphereGeometry(r, 32, 32) : new THREE.SphereGeometry(r, 8, 8);
+}
 
 //
 // cylinder
 //
 XAC.Cylinder = function(r, h, mat, openEnded) {
+	this.update(r, h, mat, openEnded)
+		// this._m = new THREE.Mesh(this._g, mat == undefined ? XAC.MATERIALNORMAL.clone() : mat.clone());
+}
+XAC.Cylinder.prototype = Object.create(XAC.Thing.prototype);
+XAC.Cylinder.prototype.update = function(r, h, mat, openEnded) {
+	this._h = h;
 	if (r.r1 != undefined && r.r2 != undefined) {
 		this._r1 = r.r1;
 		this._r2 = r.r2;
@@ -91,8 +112,10 @@ XAC.Cylinder = function(r, h, mat, openEnded) {
 		this._r1 = this._r2 = r;
 	}
 
-	this._h = h;
 	this._g = new THREE.CylinderGeometry(this._r1, this._r2, this._h, 32, 1, openEnded);
-	this._m = new THREE.Mesh(this._g, mat == undefined ? XAC.MATERIALNORMAL.clone() : mat.clone());
-}
-XAC.Cylinder.prototype = Object.create(XAC.Thing.prototype);
+	if (this._m == undefined) {
+		this._m = new THREE.Mesh(this._g, mat == undefined ? XAC.MATERIALNORMAL.clone() : mat.clone());
+	} else {
+		this._m.geometry = this._g;
+	}
+};
