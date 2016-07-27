e516e91a6d44f2a2a8309d14fad96e0fe320508d
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-07-27 17:38:02 -0400

    half way thru computing actv_elms

--- mashup/topy_server.py
+++ mashup/topy_server.py
@@ -13,6 +13,49 @@ from urlparse import urlparse, parse_qs
 import time
 import sys
 import subprocess
+import json
+import math
+
+INFINITY = 1e9
+ANALYSIS = 0
+OPTIMIZATION = 1
+
+def test():
+    if isIn([2.0, -2.0], [1.0, 1.0], [-1.0, -3.0], 1, 3):
+        print 'in'
+    return
+
+def isIn(p, p0, p1, t0, t1):
+    # check if p's projection is on p0-p1
+    v0 = [p[0]-p0[0], p[1] - p0[1]]
+    u10 = [p1[0]-p0[0], p1[1] - p0[1]]
+    l = math.sqrt(u10[0]**2 + u10[1]**2)
+    dp0 = (v0[0] * u10[0] + v0[1] * u10[1]) / l
+
+    v1 = [p[0]-p1[0], p[1] - p1[1]]
+    u01 = [p0[0]-p1[0], p0[1] - p1[1]]
+    dp1 = (v1[0] * u01[0] + v1[1] * u01[1]) / l
+
+    if dp0 > l or dp1 > l:
+        return False
+
+    # if on, check if p is in the cone given by t0, t1
+    dx1 = p0[0] - p[0]
+    dy1 = p0[1] - p[1]
+    dx2 = p1[0] - p[0]
+    dy2 = p1[1] - p[1]
+    tu = (dx2 - dx1) * dx1 + (dy2 - dy1) * dy1
+    tb = (p1[0] - p0[0])**2 + (p1[1] - p0[1])**2
+    t = -tu / tb
+
+    dist = math.sqrt(dx1**2 + dy1**2 - tu**2 / tb);
+    proj = [p0[0] + (p1[0] - p0[0]) * t, p0[1] + (p1[1] - p0[1]) * t];
+
+    lp = math.sqrt((proj[0]-p0[0])**2 + (proj[1]-p0[1])**2)
+    tp = lp/l * t0 + (1-lp/l) * t1
+
+    print proj, tp
+    return dist < tp
 
 #
 #   handling requests
@@ -43,12 +86,78 @@ class S(BaseHTTPRequestHandler):
         post_str = self.rfile.read(content_length)
         post_data = parse_qs(urlparse(self.path + post_str).query)
 
-        if 'tpd' in post_data:
-        	tpd_path = post_data['tpd'][0]
-        	# print ['python', rel_topy_path, tpd_path]
-        	subprocess.call('python ' + rel_topy_path + ' ' + tpd_path, shell=True)
-        elif 'mashup' in post_data:
-            print post_data['mashup'][0]
+        if 'mashup' not in post_data:
+            self.wfile.write('no design information')
+            return;
+
+        #
+        # read parameters of the design & function spec.
+        #
+        mashup = json.loads(post_data['mashup'][0])
+        dimension = int(mashup['dimension'][0]) if 'dimension' in mashup else 2
+        design = mashup['design'] if 'design' in mashup else None
+        loads = mashup['loads'][0] if 'loads' in mashup else None
+        clearances = mashup['clearances'][0] if 'clearances' in mashup else None
+        boundaries = mashup['boundaries'][0] if 'boundaries' in mashup else None
+
+        #
+        # read other params
+        #
+        query = int(post_data['query'][0]) if 'query' in post_data else ANALYSIS
+        resolution = int(post_data['resolution'][0]) if 'resolution' in post_data else 64
+        material = float(post_data['material'][0]) if 'material' in post_data else 0.3
+        originality = float(post_data['originality'][0]) if 'originality' in post_data else 1.0
+        verbose = int(post_data['verbose'][0]) if 'verbose' in post_data else 0
+
+        #
+        # TODO: convert everything to tpd and save it locally
+        #
+
+        # compute the resolution of the voxel grid
+        vmin = [INFINITY, INFINITY, INFINITY]
+        vmax = [-INFINITY, -INFINITY, -INFINITY]
+        for edge in design:
+            points = edge['points']
+            for point in points:
+                for i in xrange(0, 3):
+                    vmin[i] = min(vmin[i], point[i])
+                    vmax[i] = max(vmax[i], point[i])
+
+        dim_voxel = None
+        if dimension == 2:
+            dim_voxel = min((vmax[0] - vmin[0]) / resolution, (vmax[1] - vmin[1]) / resolution);
+        elif dimension == 3:
+            self.wfile.write('3d is too slow; currently unsupported')
+            return;
+
+        nelx = int(math.ceil((vmax[0] - vmin[0]) / dim_voxel))
+        nely = int(math.ceil((vmax[1] - vmin[1]) / dim_voxel))
+
+        # compute the ACTV_ELEMs
+        actv_elms = []
+        for i in xrange(0, nelx):
+            for j in xrange(0, nely):
+                x = i * dim_voxel
+                y = j * dim_voxel
+                for edge in design:
+                    points = edge['points']
+                    thickness = edge['thickness']
+                    for k in xrange(0, len(points) - 1):
+                        p0 = points[k]
+                        p1 = points[k+1]
+                        t0 = thickness[k]
+                        t1 = thickness[k+1]
+                        if isIn([x, y], p0, p1, t0, t1):
+                            actv_elms.append([i, j, 1])
+
+        tpd_path = ''
+
+        # call topy
+        out_path = ' '  # timestamped temp output path
+        # subprocess.call('python ' + rel_topy_path + ' ' + tpd_path + ' ' + query + ' ' + out_path, shell=True)
+
+        # TODO: package the result
+        self.wfile.write('roger that!')
 
 #
 #   running the server
@@ -68,6 +177,8 @@ if __name__ == "__main__":
     if len(argv) != 3:
     	print 'usage: ./topy_server.py [port num] [abs path to topy optimize.py]'
     	exit()
+
+    test()
 
     rel_topy_path = argv[2]
 
