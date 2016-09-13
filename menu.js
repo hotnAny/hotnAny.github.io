73bb801fec1c1ed38b838b630d6e88618b31e26c
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-09-12 23:33:48 -0400

    created some pages

--- menu.js
+++ menu.js
@@ -13,13 +13,8 @@ XAC.readTextFile = function(file, followup) {
 	rawFile.send(null);
 }
 
-var testText = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum."
-var testItem = {
-	imgUrl: 'smachno_03.jpg',
-	name: 'Project Name',
-	descp: 'This is an awesome project. This is an awesome project. This is an awesome project. This is an awesome project. This is an awesome project. This is an awesome project.',
-	venue: "UIST '18"
-};
+
+var divPages = [];
 var menuObj;
 
 
@@ -63,7 +58,29 @@ function makeTitle(title) {
 function makeItem(item) {
 	var tb = $('<table class="tbresearch" border="0"></table>');
 	var tr = $('<tr></tr>');
-	var tdImage = $('<td class="tdimg"><image src=' + item.imgUrl + '/></td>');
+	var imgStr = '<image src=' + item.imgUrl + ' id="tdImg_' + item.name + '" class="imgicon"/>';
+	var tdImage = $('<td class="tdimg">' + imgStr + '</td>');
+	
+	if (item.pageSrc != undefined) {
+		XAC.readTextFile(item.pageSrc, function(text){
+			// console.log(text)
+			var divPage = $('<div class="divpage"></div>');
+			divPage.html(text);
+			divPage.popup({
+				transition: 'all 0.3s'
+			});
+			divPages['tdImg_' + item.name] = divPage;
+		});
+		tdImage.css('cursor', 'pointer');
+		tdImage.click(function(e) {
+			console.log($(e.target)[0].id);
+			var divPage = divPages[$(e.target)[0].id];
+			divPage.popup('show');
+		});
+	} else if (item.exturl != undefined) {
+		tdImage = $('<td class="tdimg"><a href=' + item.exturl + ' target="_blank">' + imgStr + '</a></td>');
+	}
+
 	var tdDescp = $('<td></td>');
 
 	var strEllips = " ";
@@ -72,11 +89,7 @@ function makeItem(item) {
 		strEllips += '. '
 	}
 
-	var tbDescp = $('<table class="tbdescp" border="0"><tr>'
-		+ '<td class="tdprojname"><b>' + item.name
-		+ '</b></td>' + '<td class="tdpubvenue">'
-		+ item.venue + strEllips
-		+ '</td>' + '</tr><tr><td colspan="2"><div class="divdescp">' + item.descp + '</div></td></tr></table>');
+	var tbDescp = $('<table class="tbdescp" border="0"><tr>' + '<td class="tdprojname"><b>' + item.name + '</b></td>' + '<td class="tdpubvenue">' + item.venue + strEllips + '</td>' + '</tr><tr><td colspan="2"><div class="divdescp">' + item.descp + '</div></td></tr></table>');
 	// tdDescp.append($('<h4>' + item.name + '</h4>'));
 	// tdDescp.append($('<div>' + item.descp + '</div>'));
 	tdDescp.append(tbDescp);
