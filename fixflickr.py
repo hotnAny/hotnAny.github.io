cb285970ec05a05f1a91ad962cf5f6d0e4e1c183
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-09-18 15:18:54 -0400

    fixing text to put into json

---
+++ fixflickr.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os 
+
+if __name__ == "__main__":
+	text = open('flickr', 'r').read()
+	text = text.replace('\"', '\'')
+	# text = text.replace('500', '450')
+	# text = text.replace('281', '252')
+	print text
+	os.system("echo '%s' | pbcopy" % text)
