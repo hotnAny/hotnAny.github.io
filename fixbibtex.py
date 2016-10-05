47d4c539aa6da606a7826297c787c9238738453d
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-10-05 17:14:31 -0400

    added small fixes

--- fixbibtex.py
+++ fixbibtex.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
 	text = open('.bibtex', 'r').read()
 	text = text.replace('\n', '<br>')
 	text = text.replace('  ', '&nbsp; &nbsp; &nbsp; &nbsp;')
+	text = text.replace('\t', '&nbsp; &nbsp; &nbsp; &nbsp;')
 	text = text.replace('\&', '&amp;')
 	print text
 	os.system("echo '%s' | pbcopy" % text)
