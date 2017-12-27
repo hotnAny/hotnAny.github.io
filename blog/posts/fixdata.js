fd96afe8f879070c8cb5982ab7273bccc6aa2aa3
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-12-27 14:37:44 -0500

    tweaked image display

---
+++ blog/posts/fixdata.js
@@ -0,0 +1,40 @@
+var readYaml = require('read-yaml')
+var writeYaml = require('write-yaml')
+
+var fixflickr = function (project) {
+    if (project.flickr == undefined) return
+
+    if (project.flickr.length <= 0) {
+        delete project.flickr
+        return
+    }
+
+    var prefix = 'albums/'
+    var idxAlbumId = project.flickr.indexOf(prefix)
+    var strFlickr = project.flickr.substring(idxAlbumId + prefix.length)
+    // // console.log(strFlickr)
+    // var idxEndAlbumId = strFlickr.indexOf(" ")
+    // // console.log(idxEndAlbumId)
+    // var albumId = strFlickr.substring(0, idxEndAlbumId)
+    var albumId = ""
+    while (parseInt(strFlickr[0]) >= 0) {
+        albumId += strFlickr[0]
+        strFlickr = strFlickr.substring(1)
+    }
+    // if (albumId.length > 0)
+
+    // console.log(strFlickr)
+    console.log(albumId)
+    project.flickr = albumId
+}
+
+readYaml('../../data.yml', function (err, data) {
+    for (project of data.specials) fixflickr(project)
+    for (project of data.fabrication) fixflickr(project)
+    for (project of data.interactiontechniques) fixflickr(project)
+    for (project of data.internetofthings) fixflickr(project)
+
+    writeYaml('data.yml', data, function (err) {
+		if (err != null) console.log(err)
+	})
+})
--- js/xac.web.js
+++ js/xac.web.js
@@ -5,9 +5,9 @@ var divVideos = []
 var dirImages = 'image/'
 var col
 
-window.mobilecheck = function() {
+window.mobilecheck = function () {
 	var check = false;
-	(function(a) {
+	(function (a) {
 		if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
 	})(navigator.userAgent || navigator.vendor || window.opera)
 	return check
@@ -27,13 +27,13 @@ function checkResponsiveness() {
 	$('#divMenu').css('-webkit-column-count', col)
 }
 
-$(document).ready(function() {
-	$(window).resize(function() {
+$(document).ready(function () {
+	$(window).resize(function () {
 		checkResponsiveness()
 	});
 
 
-	YAML.load('data.yml', function(result) {
+	YAML.load('data.yml', function (result) {
 		var menuObj = result;
 
 		$('#divSpecial').append(makeTitle('HOUSE SPECIALS'))
@@ -82,13 +82,13 @@ function makeItem(item) {
 
 	if (item.title != undefined) {
 		img.css('cursor', 'pointer')
-		img.click(function(e) {
-			var htmlText = divPages[$(e.target)[0].id]
-			var divPage = $('<div class="divpage"></div>');
+		img.click(function (e) {
+			// var htmlText = divPages[$(e.target)[0].id]
+			var divItem = $('<div class="divpage"></div>');
 
-			divPage.popup({
+			divItem.popup({
 				transition: 'all 0.3s',
-				onclose: function() {
+				onclose: function () {
 					console.log(this.attr('id'))
 					var iframeId = 'v_' + this.attr('id')
 					var src = $('iframe#' + iframeId).attr('src')
@@ -96,9 +96,9 @@ function makeItem(item) {
 					$(document.body).css('overflow', 'scroll')
 				}
 			})
-			divPage.popup('show')
-			item.iframeId = 'v_' + divPage.attr('id')
-			divPage.append(makePage(item))
+			divItem.popup('show')
+			item.iframeId = 'v_' + divItem.attr('id')
+			divItem.append(makePage(item))
 
 			if (col <= 1) {
 				$('.divpage').css('width', '70%')
@@ -115,13 +115,13 @@ function makeItem(item) {
 		var embedCode = '<iframe id="ifmVideo" src="' + srcCode + '" width="960" height="540" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 		divVideos['tdImg_' + item.name] = embedCode
 		img.css('cursor', 'pointer')
-		img.click(function(e) {
+		img.click(function (e) {
 			var embedCode = divVideos[$(e.target)[0].id]
 			var divVideo = $('<div class="divvideo"></div>')
 			divVideo.html(embedCode)
 			divVideo.popup({
 				transition: 'all 0.3s',
-				onclose: function() {
+				onclose: function () {
 					$('#ifmVideo').attr('src', '')
 					$('#ifmVideo').attr('id', '_ifmVideo')
 				}
@@ -175,18 +175,37 @@ function makePage(item) {
 	//
 	// album
 	//
-	var divPhotos = $('<div class="divphotos"><div>')
-	var walbum = col > 2 ? 640 : window.innerWidth * 0.5
-	var halbum = walbum * 9 / 16;
+	// var divPhotos = $('<div class="divphotos"><div>')
+	// var walbum = col > 2 ? 640 : window.innerWidth * 0.5
+	// var halbum = walbum * 9 / 16;
 
 	if (item.flickr != undefined) {
-		item.flickr = item.flickr.replace('500', walbum)
-		item.flickr = item.flickr.replace('281', halbum)
-		divPhotos.html(item.flickr)
+		// var prefix = 'albums/'
+		// var idxAlbumId = item.flickr.indexOf(prefix)
+		// var strFlickr = item.flickr.substring(idxAlbumId + prefix.length)
+		// console.log(strFlickr)
+		// var idxEndAlbumId = Math.min(Math.min(strFlickr.indexOf("'"), 
+		// strFlickr.indexOf("\n")), strFlickr.indexOf(" "))
+		// console.log(idxEndAlbumId)
+		// var albumId = strFlickr.substring(0, idxEndAlbumId)
+		// console.log(albumId)
+
+		// 	item.flickr = item.flickr.replace('500', walbum)
+		// 	item.flickr = item.flickr.replace('281', halbum)
+		// 	divPhotos.html(item.flickr)
+		// 	divPage.append(divPhotos)
+
+
+		var divPhotos = $('<div id="flickrembed"></div><div style="position:absolute; top:-70px; display:block; text-align:center; z-index:-1;"></div><script src="https://flickrembed.com/embed_v2.js.php?source=flickr&layout=responsive&input=' + item.flickr + '&sort=0&by=album&theme=default_notextpanel&scale=fit&limit=10&skin=default-light&autoplay=false"></script>')
+
+		// var divPhotos = $('<iframe style="position: relative; top: 0; left: 0; width: 100%; height: 100%;" src="https://flickrembed.com/cms_embed.php?source=flickr&layout=responsive&input=' + item.flickr + '&sort=0&by=album&theme=default_notextpanel&scale=fit&limit=10&skin=default-light&autoplay=false" scrolling="no" frameborder="0" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe><div style="position: absolute; padding-bottom: 60%; overflow: auto; -webkit-overflow-scrolling:touch;"><small style="display: block; text-align: center; position: absolute; bottom: 0; left: 0; right: 0; margin-left: auto; margin-right: auto;"></div><div style="position:absolute; top:-70px; display:block; text-align:center; z-index:-1;"> </div>')
+
 		divPage.append(divPhotos)
+
+		divPage.append($('<br/><br/>'))
 	}
 
-	divPage.append($('<br/>'))
+
 	//
 	// pub & bib tex
 	//
